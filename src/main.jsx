20f8e5cc0baf51758c0aab5525a9f6dbbc8aaea6
aaboabdala896-ui <aaboabdala896@gmail.com>
2026-09-09 16:59:36 +0300

    feat: initialize Web Push client

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./modern.css";
+import { initPushNotifications } from "./pushNotifications.js";
+
+const stopPushInit = initPushNotifications();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => stopPushInit?.());
+}
